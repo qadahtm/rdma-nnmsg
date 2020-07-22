6aa26e599e6c852c04506ee298e7a906dbd827fb
Shubham Pandey <16755241+p-shubham@users.noreply.github.com>
2020-07-21 22:20:46 -0700

    One node fails to receive all the messages

--- bus.cpp
+++ bus.cpp
@@ -7,6 +7,7 @@
 #include <set>
 #include <iterator>
 #include <string.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -14,53 +15,73 @@ int ports[5] = {17000,17001,17002,17003,17004};
 
 int node(const int argc, const char **argv)
 {   
-    set <std::string> setofids, ended;
-    int sock = nn_socket(AF_SP, NN_BUS);
-    char myurl[22];
+    set <std::string> setofids, acks;
+
+    char myurl_o[22], myurl_e[22];
     int id = atoi(argv[1]);
-    snprintf(myurl, 22,"tcp://127.0.0.1:%d",ports[id]);
-    assert(sock >= 0);
-    assert(nn_bind(sock, myurl) >= 0);
+    int to = 100,count = 0;
+    snprintf(myurl_o, 22,"tcp://127.0.0.1:%d",ports[id]);
+    snprintf(myurl_e, 22,"tcp://127.0.0.1:%d",10 + ports[id]);
+
+    int sock_odd = nn_socket(AF_SP, NN_BUS);
+    assert(sock_odd >= 0);
+    int sock_even = nn_socket(AF_SP, NN_BUS);
+    assert(sock_even >= 0);
+    assert(nn_bind(sock_odd, myurl_o) >= 0);
+    assert(nn_bind(sock_even, myurl_e) >= 0);
+
+    assert(nn_setsockopt(sock_odd, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof(to)) >= 0);
+    assert(nn_setsockopt(sock_odd, NN_SOL_SOCKET, NN_SNDTIMEO, &to, sizeof(to)) >= 0);
+    assert(nn_setsockopt(sock_even, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof(to)) >= 0);
+    assert(nn_setsockopt(sock_even, NN_SOL_SOCKET, NN_SNDTIMEO, &to, sizeof(to)) >= 0);
+
+    sleep(10);
     for(int i = 0;i < 5; i++){
         if(id == i){
             continue;
         }
         char url[22];
-        snprintf(url, 22,"tcp://127.0.0.1:%d",ports[i]);
-        assert(nn_connect(sock, url) >= 0);
+        if(i % 2 == 0){
+            snprintf(url, 22,"tcp://127.0.0.1:%d",10 + ports[i]);
+            assert(nn_connect(sock_odd, url) >= 0);
+        }
+        else{
+            snprintf(url, 22,"tcp://127.0.0.1:%d",ports[i]);
+            assert(nn_connect(sock_odd, url) >= 0);
+        }
     }
-    int to = 100,count = 0;
-    assert(nn_setsockopt(sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof(to)) >= 0);
+    sleep(10);
     int sz_n = strlen(argv[1]) + 1;
     printf("%s: SENDING '%s' ONTO BUS\n", argv[1], argv[1]);
     // int send = nn_send(sock, argv[1], sz_n, 0);
     // assert(send == sz_n);
     printf("Sent\n");
-    while (setofids.size() < 4 && ended.size() <= 4)
+    while (setofids.size() < 4)
     {
-        char *buf = NULL;
-        int recv = nn_recv(sock, &buf, NN_MSG, 0);
-        if (recv >= 0 && buf != "END")
+        char *buf_o = NULL;
+        char *buf_e = NULL;
+
+        int recv = nn_recv(sock_odd, &buf_o, NN_MSG, 0);
+        if (recv >= 0)
         {   
-            setofids.insert(buf);
-            nn_freemsg(buf);
+            setofids.insert(buf_o);
+            nn_freemsg(buf_o);
         }
-        else if(recv >= 0 && buf == "END"){
-            ended.insert("END");
+        int send = nn_send(sock_odd, argv[1], sz_n, 0);
+        assert(send == sz_n);
+
+        recv = nn_recv(sock_even, &buf_e, NN_MSG, 0);
+        if (recv >= 0)
+        {   
+            setofids.insert(buf_e);
+            nn_freemsg(buf_e);
         }
-        if(setofids.size()< 4){
-            int send = nn_send(sock, argv[1], sz_n, 0);
-            assert(send == sz_n);
-        }
-        else{
-            char *bufs = strcat((char *)argv[1], "END");
-            int send = nn_send(sock, bufs, sizeof("END"), 0);
-            assert(send == sz_n);
-        }
+        send = nn_send(sock_even, argv[1], sz_n, 0);
+        assert(send == sz_n);
     }
     for (std::set<std::string>::iterator it=setofids.begin(); it!=setofids.end(); ++it)
         std::cout << ' ' << *it;
-    return nn_shutdown(sock, 0);
+    return nn_shutdown(sock_odd, 0);
 }
 
 int main(const int argc, const char **argv)
